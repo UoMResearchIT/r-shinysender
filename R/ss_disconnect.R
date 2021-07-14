a48c3da1d9d358447d4410d4b008a441a73d6b10
David Mawdsley <david.mawdsley@manchester.ac.uk>
2021-07-14 13:32:48 +0100

    Function to disconnect session

---
+++ R/ss_disconnect.R
@@ -0,0 +1,13 @@
+#' Disconnect from Shiny server
+#'
+#' Disconnect a session from the Shiny server
+#'
+#' @param session The session to disconnect
+#'
+#' @export
+ss_disconnect <- function(session){
+
+  ssh::ssh_disconnect(session)
+
+
+}
