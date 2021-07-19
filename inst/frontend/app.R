912c809f4db39d33552ddd7aa39d884ea63fa202
David Mawdsley <david.mawdsley@manchester.ac.uk>
2021-07-19 09:53:42 +0100

    Add note on modals

--- inst/frontend/app.R
+++ inst/frontend/app.R
@@ -10,6 +10,8 @@ ui <- dashboardPage(dashboardHeader(title = "Shiny Sender"),
 
 server <- function(input, output) {
 
+# https://stackoverflow.com/a/50326449/1683372 details of how to do
+# modals in Shiny server
 
 }
 
