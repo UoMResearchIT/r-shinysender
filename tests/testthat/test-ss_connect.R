bcadb86431a452b3d9947070ebbaa639c6d6e770
David Mawdsley <david.mawdsley@manchester.ac.uk>
2021-07-30 13:59:49 +0100

    Test username detection code

--- tests/testthat/test-ss_connect.R
+++ tests/testthat/test-ss_connect.R
@@ -9,3 +9,40 @@ test_that("We error if host does not resolve", {
                "Failed to resolve hostname")
 
 })
+
+
+test_that("Username detection works", {
+
+  if (requireNamespace("withr", quietly = TRUE)) {
+    withr::with_envvar(new = c("USER"="",
+                               "USERNAME"=""),
+                       {
+                         expect_error(getUserName(), "Could not determine username")
+                       })
+
+    withr::with_envvar(new = c("USER"="alice",
+                               "USERNAME"=""),
+                       {
+                         expect_equal(getUserName(), "alice")
+                       })
+
+    withr::with_envvar(new = c("USER"="",
+                               "USERNAME"="alice"),
+                       {
+                         expect_equal(getUserName(), "alice")
+                       })
+
+    withr::with_envvar(new = c("USER"="alice",
+                               "USERNAME"="alice"),
+                       {
+                         expect_equal(getUserName(), "alice")
+                       })
+
+    withr::with_envvar(new = c("USER"="alice",
+                               "USERNAME"="bob"),
+                       {
+                         expect_error(getUserName(), "but not equal")
+                       })
+  }
+
+})
