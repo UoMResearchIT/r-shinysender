5d1b8d1f298705020bcae084e35f3d3d83fc52f9
David Mawdsley <david.mawdsley@manchester.ac.uk>
2021-08-16 15:58:59 +0100

    Return last or list of logs for an app

---
+++ R/ss_getlogs.R
@@ -0,0 +1,57 @@
+#' Get error logs for an app from the remote server
+#'
+#' @param session The ssh session to use
+#' @param appname The name of the application to collect logs for.  Defaults to the name of your current local directory
+#' @param what One of "last", "list", or a pid as returned by list.  Defaults to "last" - the most recent log for the app
+#'
+#' @return An invisible copy of the selected logfile, or a visible data frame containing the remote log files for the app
+#'
+#' @export
+ss_getlogs <- function(session,
+                       appName = basename(getwd()),
+                       what = "last") {
+
+  alllogs <- ss_listdir(session, "~/ShinyApps/log")
+
+  # Parse the filenames into a data frame
+  proto <- data.frame(appName=character(),
+                      user=character(),
+                      date=integer(),
+                      time=integer(),
+                      pid=integer())
+  logdata <- strcapture("(\\w+)-(\\w+)-(\\d+)-(\\d+)-(\\d+)\\.log",
+                        alllogs, proto)
+  logdata <- cbind( logname=alllogs, logdata )
+
+
+  applogs <- logdata[logdata$appName == appName,]
+
+  # Sort by date and time
+  appsort <- applogs[order(paste0(applogs$date, applogs$time)),]
+
+  if(what == "list"){ # data frame of log files for the app
+    return(appsort)
+  }
+
+  if(what == "last"){ # Return most recent log for app
+    # Most recent will be last
+    wantfile <- appsort[nrow(appsort), "logname"]
+
+    logloc <- paste0("~/ShinyApps/log/", wantfile)
+
+    remotecommand <- paste0("cat ", logloc)
+
+    raw_log <- ssh::ssh_exec_internal(session,
+                                      command = remotecommand,
+                                      error = FALSE)
+
+    remote.log <- process_raw(raw_log$stdout)
+
+    message(wantfile)
+    message()
+    message(paste0(remote.log, "\n"))
+    invisible(remote.log)
+
+  }
+
+}
