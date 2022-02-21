b74eef4c2b725d039adf315f8b41ff75a4139bb1
David Mawdsley <david.mawdsley@manchester.ac.uk>
2022-02-21 13:49:21 +0000

    Test ss_connect() using mockery

--- tests/testthat/test-ss_connect.R
+++ tests/testthat/test-ss_connect.R
@@ -1,3 +1,5 @@
+library(mockery)
+
 test_that("we error if no username or server", {
   expect_error(ss_connect(username=""), "Pass username parameter")
 
@@ -8,6 +10,26 @@ test_that("we error if no username or server", {
 test_that("We error if host does not resolve", {
   expect_error(ss_connect(username="dummy", server="nothere.notdomain"),
                "Failed to resolve hostname")
+
+})
+
+
+
+# Check we get a (mocked) ssh session when we connect with valid parameters
+test_that("We get an ssh session", {
+
+  mockssh <- function(conname, keyfile){
+
+    dummysession <- "dummy ssh session"
+    class(dummysession) <- "ssh"
+
+    return(dummysession)
+
+  }
+
+  stub(ss_connect, 'ssh::ssh_connect', mockssh)
+
+  expect_s3_class(ss_connect(server= "mockserver", username = "mockuser"), "ssh")
 
 })
 
