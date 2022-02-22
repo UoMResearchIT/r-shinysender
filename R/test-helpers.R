487ed9130bc1d9b619aeb4b930fe5580f732b7a3
David Mawdsley <david.mawdsley@manchester.ac.uk>
2022-02-22 11:06:59 +0000

    Include files in local shiny app fixture

--- R/test-helpers.R
+++ R/test-helpers.R
@@ -1,12 +1,61 @@
 # Generate a minimal Shiny app in its own directory
+# Based on https://testthat.r-lib.org/articles/test-fixtures.html
 
-create_local_shiny_app <- function(dir = fs::file_temp(), env = parent.frame()) {
+create_local_shiny_app  <- function(dir = fs::file_temp(), env = parent.frame(), singlefile = TRUE) {
+  # create new folder and package
+  dir.create(dir) # A
+  withr::defer(fs::dir_delete(dir), envir = env) # -A
 
-  fs::dir_create(dir)
-  withr::defer(fs::dir_delete(dir), envir = env)
+  old_project_dir <- getwd() # B
+  setwd(dir)
+  ui.R <- c(
+    'library(shiny)',
+    'shinyUI(fluidPage(',
+    'titlePanel("Old Faithful Geyser Data"),',
+    'sidebarLayout(',
+    'sidebarPanel(',
+    'sliderInput("bins",',
+    '"Number of bins:",',
+    'min = 1,',
+    'max = 50,',
+    'value = 30)',
+    '),',
+    'mainPanel(',
+    'plotOutput("distPlot")',
+    ')',
+    ')',
+    '))'
+  )
 
-  # TODO make a plausible shiny app
-  fs::file_create(paste0(dir, "/app.R"))
+  server.R <- c(
+    'library(shiny)',
+    'shinyServer(function(input, output) {',
+    'output$distPlot <- renderPlot({',
+    'x    <- faithful[, 2]',
+    'bins <- seq(min(x), max(x), length.out = input$bins + 1)',
+    'hist(x, breaks = bins, col = "darkgray", border = "white")',
+    '})',
+    '})'
+  )
+
+  # Spit out to multiple / single files
+  if(!singlefile) {
+    uiConn <- file("ui.R")
+    serverConn <- file("server.R")
+  } else {
+    uiConn <- file("app.R")
+    serverConn <- uiConn
+  }
+
+  writeLines(ui.R, uiConn)
+  writeLines(server.R, serverConn)
+
+
+  close(uiConn)
+  if(!singlefile) # Can't close a file twice
+    close(serverConn)
+
+  withr::defer(setwd(old_project_dir), envir = env) # -B
 
 
 
