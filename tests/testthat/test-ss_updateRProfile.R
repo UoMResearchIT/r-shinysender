f02ca294ff9967083bcac0dabd1870233de211d8
David Mawdsley <david.mawdsley@manchester.ac.uk>
2022-02-22 13:28:58 +0000

    More tests on updating .Rprofile

--- tests/testthat/test-ss_updateRProfile.R
+++ tests/testthat/test-ss_updateRProfile.R
@@ -1,5 +1,6 @@
-test_that("We can find Rprofile fragment", {
+test_that("We can find Rprofile fragments", {
  expect_match(ShinySenderRprofilePath(), "shinysender_Rprofile")
+ expect_match(ShinySenderRprofilePathStaging(), "shinysender_staging_Rprofile")
 })
 
 test_that("RProfile updating works", {
@@ -31,5 +32,46 @@ test_that("RProfile updating works", {
 
 
   expect_error(shinysenderize_Rprofile(c(1,2,3)))
+
+})
+
+test_that("We can convert application names to paths", {
+
+  expect_equal(appnameToPath("testapp"), "~/ShinyApps/testapp")
+  expect_error(appnameToPath("*badappname"))
+
+})
+
+
+test_that("We can handle remote rprofiles", {
+  fakesession <- fakessh()
+  expect_error(get_remote_Rprofile(fakesession, appname = NULL, remotepath = NULL),
+               "Must specify appname or remotepath, not both")
+
+  expect_error(get_remote_Rprofile(fakesession, appname = "myapp", remotepath = "~/mypath"),
+               "Must specify appname or remotepath, not both")
+
+})
+
+
+test_that("We can detect a missing remote Rprofile", {
+
+  mock_failed_ssh_command <- function(session, command, error) {
+    myobject <- list()
+    myobject$status <- 1
+
+    return(myobject)
+  }
+
+  fakesession <-  fakessh()
+
+  stub(get_remote_Rprofile, 'ssh::ssh_exec_internal', mock_failed_ssh_command )
+
+  expect_error(
+    expect_message(get_remote_Rprofile(fakesession, appname = "myapp",
+                                     warnmissing = TRUE),
+                 "No remote \\.Rprofile found"), # Error we care about
+    "argument 'x' must be a raw vector") # Error from not having a real ssh connection
+
 
 })
